c8040b27829f51288c722a73762f2bde052a76a4
karapeneguy <helpdesk@helps-imac-206.smc.loyno.edu>
2018-04-19 10:36:10 -0500

    Added Scripts to footer
    Added Scripts to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,19 @@
-</body>
 
-    
-    <footer>
+
+<script src="assets/scripts/main.js"></script>
+
+<footer>
     <div class="blackboxfooter">
         
         <h2>© kara wolfe 2018</h2>
         </div>
     </footer>
 
+
+</body>
+
+    
+    
 
 
 
