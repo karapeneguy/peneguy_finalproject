7b20c3c635d9833f949d45c99114cb37601b3630
karapeneguy <helpdesk@helps-imac-206.smc.loyno.edu>
2018-04-19 10:21:28 -0500

    PHP and Folders
    Added PHP files and folders!

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include "templates/header.php"; ?>
+
+   
+<img src="images/bluewallskirt.jpg" id="mainimage">
+    
+        <img src="images/IMG_20171029_180205.jpg" id="aboutmeimg">
+
+    
+   <div id="aboutme"> <h3>About Me</h3>
+    
+   <p id="trait">•vulnerable•naive•shy•quiet•reserved•raw•</p>
+       
+    <p id="trait">•honest•bare•nervous•intimate•sensual•</p>
+    <p id="trait">strong•bold•brave•daring•fiery•intense•fierce•</p>
+    <p id="trait">•passionate•emotional•sensitive•impulsive•</p>
+    <p id="trait">•temperamental•headstrong•stubborn•highstrung•</p>
+    <p id="trait">•impatient•restless•reclusive•solitary•sweet•</p>
+    <p id="trait">•welcoming•respectful•selfconscious•awkward•anxious•</p>
+    <p id="trait">•silly•ridiculous•illogical•creative•artsy•indecisive•complicated•</p>
+    <h3  id="aboutmetext">IMPERFECT. COMPLEX. PARADOXICAL. </h3>
+    <h3>HUMAN. </h3>
+       
+         </div>
+    
+
+    
+ 
+    <iframe id="spotifyplayerhome" src="https://open.spotify.com/embed/album/4XTUXze7zKi02cUYptnsZC" width="450" height="407" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+
+    
+    <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="Electric Swandive" data-display-local-dates="true" data-display-past-dates="true" data-auto-style="false" data-text-color="#000000" data-link-color="#000000" data-popup-background-color="#FFFFFF" data-background-color="#FFFFFF" data-display-limit="15" data-link-text-color="#FFFFFF" data-widget-width="80%"></a>
+
+    
+    
+  
+    
+    
+
+    <h1 id="signuptext">Keep up!</h1>
+    
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	
+</style>
+<div id="mc_embed_signup">
+<form action="https://karawolfe.us17.list-manage.com/subscribe/post?u=324a95b65a43c5379cf7c8951&amp;id=78b5268598" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_324a95b65a43c5379cf7c8951_78b5268598" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    
+    <script src="scripts.js"></script>
+
+
+<?php include "templates/footer.php"; ?>
---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+</body>
+
+    
+    <footer>
+    <div class="blackboxfooter">
+        
+        <h2>© kara wolfe 2018</h2>
+        </div>
+    </footer>
+
+
+
+
+
+</html>
